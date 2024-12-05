eea68ee08ce0f6a368813e3b3d896afd5db4e33f
Ivoryredgrave <ivoryredgrave@gmail.com>
2024-12-04 20:03:43 -0400

    Minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base:"https://ivoryredgrave.github.io/rickAndMortyApp"
+    base:"https://ivoryredgrave.github.io/rickandmortyapp"
 })
